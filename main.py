b9447d1968c44dc3ebcf27d99d94230b61cc932d
seungjun58 <109563310+seungjun58@users.noreply.github.com>
2022-07-21 10:22:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 
 url = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Aatrox.json'
+url2 = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Ahri.json'
 
 res = requests.get(url).json()
+res2 = requests.get(url2).json()
 
 champ_lst = (res['data']['Aatrox']['key'], res['data']['Aatrox']['name'])
+champ_lst2 = (res2['data']['Ahri']['key'], res2['data']['Ahri']['name'])
