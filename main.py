1cad2e203c8fa6616cb66ab91450fe3a89973c02
seungjun58 <jun5891@naver.com>
2022-07-21 10:18:57 +0900

    champ_name - 아트록스를 만들었음

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+
+url = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Aatrox.json'
+
+res = requests.get(url).json()
+
+champ_lst = (res['data']['Aatrox']['key'], res['data']['Aatrox']['name'])
